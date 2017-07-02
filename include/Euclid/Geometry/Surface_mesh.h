c9655eeace572fb990789a39e4baf558b83b3bc8
王少东 <wangsd@ios.ac.cn>
2017-07-02 15:27:51 +0100

    Add PropertyMap template for Surface_mesh

--- include/Euclid/Geometry/Surface_mesh.h
+++ include/Euclid/Geometry/Surface_mesh.h
@@ -5,6 +5,7 @@
 #pragma once
 #include <CGAL/Surface_mesh.h>
 #include <Eigen/Dense>
+#include <boost/property_map/property_map.hpp>
 #include <vector>
 
 namespace Euclid
@@ -21,6 +22,30 @@ bool build_surface_mesh(
 	const std::vector<Eigen::Matrix<FT, 3, 1>>& vertices,
 	const std::vector<unsigned>& indices,
 	CGAL::Surface_mesh<Point_3>& mesh);
+
+template<typename ValueType>
+class SM_PropertyMap
+{
+public:
+	using value_type = ValueType;
+	using reference = const value_type&;
+	using key_type = size_t;
+	using category = boost::lvalue_property_map_tag;
+
+	explicit SM_PropertyMap(const std::vector<value_type>& values)
+		: _values(values) {}
+	reference operator[](key_type key) const
+	{
+		return _values[key];
+	}
+	friend reference get(SM_PropertyMap pmap, key_type key)
+	{
+		return pmap[key];
+	}
+
+private:
+	const std::vector<value_type>& _values;
+};
 
 } // namespace Euclid
 
