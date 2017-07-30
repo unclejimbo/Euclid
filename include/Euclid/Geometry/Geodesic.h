749a34ae0c3f25458737ba8caf00f1af909053d7
王少东 <wangsd@ios.ac.cn>
2017-07-31 00:36:28 +0100

    Alter interface

--- include/Euclid/Geometry/Geodesic.h
+++ include/Euclid/Geometry/Geodesic.h
@@ -10,11 +10,12 @@ namespace Euclid
 
 // Single-source approximated geodesic distance using heat method
 template<typename Mesh>
-std::vector<typename CGAL::Kernel_traits<typename boost::property_traits<
-	typename boost::property_map<Mesh, boost::vertex_point_t>::type>::value_type>::Kernel::FT>
-heat_method(
+bool heat_method(
 	const typename boost::graph_traits<const Mesh>::vertex_descriptor& v,
-	const Mesh& mesh);
+	const Mesh& mesh,
+	std::vector<typename CGAL::Kernel_traits<typename boost::property_traits<
+		typename boost::property_map<Mesh, boost::vertex_point_t>::type>
+		::value_type>::Kernel::FT>& geodesics);
 
 } // namespace Euclid
 
--- include/Euclid/Geometry/src/HeatMethod.cpp
+++ include/Euclid/Geometry/src/HeatMethod.cpp
@@ -1,20 +1,21 @@
 #include <Euclid/Geometry/MeshProperties.h>
 #include <Euclid/Geometry/KernelGeometry.h>
+#include <Euclid/Util/Assert.h>
 #include <Eigen/Dense>
 #include <Eigen/SparseCholesky>
 #include <unordered_map>
 #include <iostream>
-#include <cassert>
 
 namespace Euclid
 {
 
 template<typename Mesh>
-std::vector<typename CGAL::Kernel_traits<typename boost::property_traits<
-	typename boost::property_map<Mesh, boost::vertex_point_t>::type>::value_type>::Kernel::FT>
-heat_method(
+bool heat_method(
 	const typename boost::graph_traits<const Mesh>::vertex_descriptor& v,
-	const Mesh& mesh)
+	const Mesh& mesh,
+	std::vector<typename CGAL::Kernel_traits<typename boost::property_traits<
+		typename boost::property_map<Mesh, boost::vertex_point_t>::type>
+		::value_type>::Kernel::FT>& geodesics)
 {
 	using FT = typename CGAL::Kernel_traits<typename boost::property_traits<
 		typename boost::property_map<Mesh, boost::vertex_point_t>::type>::value_type>::Kernel::FT;
@@ -23,8 +24,7 @@ heat_method(
 	auto vpmap = get(boost::vertex_point, mesh);
 	auto vimap = get(boost::vertex_index, mesh);
 	auto fimap = get(boost::face_index, mesh);
-	std::vector<FT> geodesic(num_vertices(mesh));
-	geodesic[vimap[v]] = 1.0;
+	geodesics.resize(num_vertices(mesh));
 
 	// Construct the heat equation
 	FT time_interval = 0.0;
@@ -44,24 +44,28 @@ heat_method(
 	heat_solver.compute(heat_mat);
 	if (heat_solver.info() != Eigen::Success) {
 		std::cerr << "Unable to solve the heat flow" << std::endl;
-		return geodesic;
+		return false;
 	}
 	Eigen::Matrix<FT, Eigen::Dynamic, 1> heat = heat_solver.solve(delta);
 	if (heat_solver.info() != Eigen::Success) {
 		std::cerr << "Unable to solve the heat flow" << std::endl;
-		return geodesic;
+		return false;
 	}
 
 	// Evaluate the gradient field
 	VVMap vertex_values;
 	int idx = 0;
+	FT max_heat = 0.0;
 	for (const auto& v : vertices(mesh)) {
-		assert(heat(idx) <= 1.0 && heat(idx) >= 0.0);
+		EASSERT(heat(idx) <= 1.0 && heat(idx) >= 0.0);
+		max_heat = std::max(max_heat, heat(idx));
 		vertex_values.emplace(v, heat(idx++));
 	}
+	EASSERT(max_heat == heat(vimap[v]));
 	auto gradients = gradient_field(mesh, boost::const_associative_property_map<VVMap>(vertex_values));
 	for (auto& g : gradients) {
-		g /= length(g);
+		normalize(g);
+		g = -g;
 	}
 
 	// Construct the poisson equation
@@ -69,7 +73,7 @@ heat_method(
 	for (const auto& v : vertices(mesh)) {
 		FT divergence = 0.0;
 		auto p = vpmap[v];
-		for (const auto& he : halfedges_around_target(halfedge(v, mesh), mesh)) {
+		for (const auto& he : halfedges_around_target(v, mesh)) {
 			auto f = face(he, mesh);
 			auto g = gradients[fimap[f]];
 			auto vi = source(he, mesh);
@@ -87,18 +91,19 @@ heat_method(
 	poisson_solver.compute(cot_mat);
 	if (poisson_solver.info() != Eigen::Success) {
 		std::cerr << "Unable to solve the poisson equation" << std::endl;
-		return geodesic;
+		return false;
 	}
 	Eigen::Matrix<FT, Eigen::Dynamic, 1> geod = poisson_solver.solve(divs);
 	if (poisson_solver.info() != Eigen::Success) {
 		std::cerr << "Unable to solve the poisson equation" << std::endl;
-		return geodesic;
+		return false;
 	}
 	
 	for (size_t i = 0; i < num_vertices(mesh); ++i) {
-		geodesic[i] = geod(i, 0);
+		geodesics[i] = geod(i, 0) - geod(vimap[v], 0);
+		EASSERT(geodesics[i] >= 0.0);
 	}
-	return geodesic;
+	return true;
 }
 
 } // namespace Euclid
