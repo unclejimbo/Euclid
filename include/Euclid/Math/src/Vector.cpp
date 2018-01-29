460da4ebac7fe2b0f7b5844d486eaea36ac0a072
unclejimbo <wangsd@ios.ac.cn>
2018-01-29 23:19:09 +0800

    Fix floating point compare.

--- include/Euclid/Math/src/Vector.cpp
+++ include/Euclid/Math/src/Vector.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include <CGAL/Kernel/global_functions.h>
+#include <Euclid/Math/Numeric.h>
 
 namespace Euclid
 {
@@ -16,12 +17,13 @@ inline typename Kernel::FT length(const CGAL::Vector_3<Kernel>& vec)
 template<typename Kernel>
 inline void normalize(CGAL::Vector_3<Kernel>& vec)
 {
+    using FT = typename Kernel::FT;
     auto l = Euclid::length(vec);
-    if (l > 0.0) {
-        vec /= Euclid::length(vec);
+    if (!Euclid::eq_abs_err(
+            l, static_cast<FT>(0.0), std::numeric_limits<FT>::epsilon() * 10)) {
+        vec /= l;
     }
     else {
-
         std::cerr << "Can't normalize zero vector" << std::endl;
     }
 }
@@ -29,9 +31,11 @@ inline void normalize(CGAL::Vector_3<Kernel>& vec)
 template<typename Kernel>
 inline CGAL::Vector_3<Kernel> normalized(const CGAL::Vector_3<Kernel>& vec)
 {
+    using FT = typename Kernel::FT;
     auto l = Euclid::length(vec);
     auto rtn = vec;
-    if (l > 0.0) {
+    if (!Euclid::eq_abs_err(
+            l, static_cast<FT>(0.0), std::numeric_limits<FT>::epsilon() * 10)) {
         rtn /= l;
     }
     else {
@@ -64,7 +68,12 @@ typename Kernel::FT sine(const CGAL::Vector_3<Kernel>& v1,
     FT sin = 0.0;
     auto l1 = length(v1);
     auto l2 = length(v2);
-    if (l1 != 0.0 && l2 != 0.0) {
+    if (!Euclid::eq_abs_err(l1,
+                            static_cast<FT>(0.0),
+                            std::numeric_limits<FT>::epsilon() * 10) &&
+        !Euclid::eq_abs_err(l2,
+                            static_cast<FT>(0.0),
+                            std::numeric_limits<FT>::epsilon() * 10)) {
         auto outer_prod = CGAL::cross_product(v1, v2);
         sin = std::min(length(outer_prod) / (l1 * l2), static_cast<FT>(1.0));
     }
@@ -87,7 +96,12 @@ typename Kernel::FT cosine(const CGAL::Vector_3<Kernel>& v1,
     FT cos = 1.0;
     auto l1 = length(v1);
     auto l2 = length(v2);
-    if (l1 != 0.0 && l2 != 0.0) {
+    if (!Euclid::eq_abs_err(l1,
+                            static_cast<FT>(0.0),
+                            std::numeric_limits<FT>::epsilon() * 10) &&
+        !Euclid::eq_abs_err(l2,
+                            static_cast<FT>(0.0),
+                            std::numeric_limits<FT>::epsilon() * 10)) {
         cos = v1 * v2 / (l1 * l2);
         cos = std::clamp(cos, static_cast<FT>(-1.0), static_cast<FT>(1.0));
     }
@@ -110,12 +124,19 @@ typename Kernel::FT tangent(const CGAL::Vector_3<Kernel>& v1,
     FT tan = 0.0;
     auto l1 = v1.squared_length();
     auto l2 = v2.squared_length();
-    if (l1 != 0.0 && l2 != 0.0) {
+    if (!Euclid::eq_abs_err(l1,
+                            static_cast<FT>(0.0),
+                            std::numeric_limits<FT>::epsilon() * 10) &&
+        !Euclid::eq_abs_err(l2,
+                            static_cast<FT>(0.0),
+                            std::numeric_limits<FT>::epsilon() * 10)) {
         auto inner_prod = v1 * v2;
         auto squared_cos = inner_prod * inner_prod / (l1 * l2);
         squared_cos = std::min(squared_cos, static_cast<FT>(1.0));
         tan =
-            squared_cos == 0.0
+            Euclid::eq_abs_err(squared_cos,
+                               static_cast<FT>(0.0),
+                               std::numeric_limits<FT>::epsilon() * 10)
                 ? NAN
                 : std::sqrt((static_cast<FT>(1.0) - squared_cos) / squared_cos);
         tan = inner_prod < 0.0 ? -tan : tan;
@@ -139,12 +160,17 @@ typename Kernel::FT cotangent(const CGAL::Vector_3<Kernel>& v1,
     FT cot = NAN;
     auto l1 = v1.squared_length();
     auto l2 = v2.squared_length();
-    if (l1 != 0.0 && l2 != 0.0) {
+    if (!Euclid::eq_abs_err(l1,
+                            static_cast<FT>(0.0),
+                            std::numeric_limits<FT>::epsilon() * 10) &&
+        !Euclid::eq_abs_err(l2,
+                            static_cast<FT>(0.0),
+                            std::numeric_limits<FT>::epsilon() * 10)) {
         auto inner_prod = v1 * v2;
         auto squared_cos = inner_prod * inner_prod / (l1 * l2);
         squared_cos = std::min(squared_cos, static_cast<FT>(1.0));
         cot =
-            squared_cos == 1.0
+            Euclid::eq_almost(squared_cos, static_cast<FT>(1.0))
                 ? NAN
                 : std::sqrt(squared_cos / (static_cast<FT>(1.0) - squared_cos));
         cot = inner_prod < 0.0 ? -cot : cot;
