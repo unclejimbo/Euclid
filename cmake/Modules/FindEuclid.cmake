35a427e54e44fffd78a631297a3d337a9c7e06c2
unclejimbo <wangsd@ios.ac.cn>
2018-03-27 12:38:36 +0800

    Fix find module for Euclid.

--- cmake/Modules/FindEuclid.cmake
+++ cmake/Modules/FindEuclid.cmake
@@ -5,13 +5,9 @@
 #  Euclid_FOUND
 #  Euclid_INCLUDE_DIR
 
-set(Euclid_INCLUDE_SEARCH_PATHS
+find_path(Euclid_INCLUDE_DIR Euclid/Util/Assert.h PATHS
   /usr/include
-  /usr/local/include
-  $ENV{Euclid_HOME}/include
-)
-
-find_path(Euclid_INCLUDE_DIR Euclid/Geometry/Polyhedron_3.h PATHS ${Euclid_INCLUDE_SEARCH_PATHS})
+  /usr/local/include)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Euclid DEFAULT_MSG Euclid_INCLUDE_DIR)
