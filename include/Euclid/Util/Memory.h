48b78ebd144d6f134ef8fb31878c6d1f6ce1797d
unclejimbo <wangsd@ios.ac.cn>
2018-08-14 22:11:08 +0800

    Add memory package.

---
+++ include/Euclid/Util/Memory.h
@@ -0,0 +1,198 @@
+/** Memory utilities.
+ *
+ *  This pacakge provides more memory managing facilities.
+ *  @defgroup PkgMemory Memory
+ *  @ingroup PkgUtil
+ */
+#pragma once
+
+#include <memory>
+#include <utility>
+
+namespace Euclid
+{
+
+/** A potentially resource owning pointer.
+ *
+ *  This class wraps up raw pointers who might or might not own the underlying
+ *  resource. The motivation of this design is that when constructing complex
+ *  algorithms one may need to use a lot of components, and some of them may
+ *  share commonn internal computations. So it's desired that they are computed
+ *  once and used everywhere as long as the user kept the underlying resource
+ *  valid. This class will automatically manage the resource in one place and
+ *  free the data when it goes out of scope.
+ */
+template<typename T>
+class ProPtr
+{
+public:
+    using pointer = T*;
+    using element_type = T;
+
+public:
+    /** Default constructor.
+     *
+     */
+    constexpr ProPtr() noexcept : _data(nullptr), _own(false) {}
+
+    /** Construct from nullptr_t.
+     *
+     */
+    constexpr ProPtr(std::nullptr_t) noexcept : _data(nullptr), _own(false) {}
+
+    /** Construct from pointer
+     *
+     *  @param p The raw pointer.
+     *  @param own Whether to own the underlying memory. Default is false and
+     *  this class acts just like a raw pointer. Set to true to act like a
+     *  unique_ptr.
+     */
+    explicit ProPtr(pointer p, bool own = false) noexcept : _data(p), _own(own)
+    {}
+
+    /** Non-copyable.
+     *
+     */
+    ProPtr(const ProPtr& pro) = delete;
+
+    /** Move constructor.
+     *
+     */
+    ProPtr(ProPtr&& pro) noexcept { pro.swap(*this); }
+
+    /** Move construct from a derived type.
+     *
+     */
+    template<typename U>
+    ProPtr(ProPtr<U>&& pro) noexcept
+    {
+        auto owning = pro.owns();
+        auto data = pro.release();
+        ProPtr<T> tmp(data, owning);
+        tmp.swap(*this);
+    }
+
+    /** Desctructor.
+     *
+     *  Free the memory if owns.
+     */
+    ~ProPtr()
+    {
+        if (_own) { delete _data; }
+    }
+
+    /** Non-copyable.
+     *
+     */
+    ProPtr& operator=(const ProPtr& pro) = delete;
+
+    /** Copy assignment from nullptr.
+     *
+     *  Equals to call reset(nullptr).
+     */
+    ProPtr& operator=(std::nullptr_t) noexcept
+    {
+        reset();
+        return *this;
+    }
+
+    /** Move assignment operator.
+     *
+     */
+    ProPtr& operator=(ProPtr&& pro) noexcept
+    {
+        pro.swap(*this);
+        return *this;
+    }
+
+    /** Move assignment from a derived type.
+     *
+     */
+    template<typename U>
+    ProPtr& operator=(ProPtr<U>&& pro) noexcept
+    {
+        auto owning = pro.owns();
+        auto data = pro.release();
+        ProPtr<T> tmp(data, owning);
+        tmp.swap(*this);
+        return *this;
+    }
+
+    /** Release the ownership(if owns) and return the data.
+     *
+     */
+    pointer release() noexcept
+    {
+        pointer result = nullptr;
+        std::swap(result, _data);
+        _own = false;
+        return result;
+    }
+
+    /** Reset the ProPtr.
+     *
+     *  **Note**
+     *
+     *  Self-reset is not checked. Double owning is not checked either.
+     *  Proceed with caution.
+     */
+    void reset(pointer p = pointer(), bool own = false) noexcept
+    {
+        if (_own) { delete _data; }
+        _data = p;
+        _own = own;
+    }
+
+    /** Swap this ProPtr with another, both resource and ownership.
+     *
+     */
+    void swap(ProPtr& pro) noexcept
+    {
+        using std::swap;
+        swap(pro._data, _data);
+        swap(pro._own, _own);
+    }
+
+    /** Get the underlying resource.
+     *
+     */
+    pointer get() const noexcept { return _data; }
+
+    /** Return true if resource is valid.
+     *
+     */
+    explicit operator bool() const noexcept { return _data; }
+
+    /** Return the data being pointed to.
+     *
+     */
+    typename std::add_lvalue_reference<T>::type operator*() const
+    {
+        return *_data;
+    }
+
+    /** Return the raw pointer.
+     *
+     */
+    pointer operator->() const noexcept { return _data; }
+
+    /** Return whether this pointer owns the resource.
+     *
+     */
+    bool owns() const noexcept { return _own; }
+
+private:
+    pointer _data;
+    bool _own;
+};
+
+/** Swap contents of two ProPtr<T>s.
+ *
+ */
+template<typename T>
+void swap(ProPtr<T>& lhs, ProPtr<T>& rhs)
+{
+    lhs.swap(rhs);
+}
+
+} // namespace Euclid
---
+++ test/Util/test_Memory.cpp
@@ -0,0 +1,173 @@
+#include <catch.hpp>
+#include <Euclid/Util/Memory.h>
+
+#ifndef NDEBUG
+#include <iostream>
+#endif
+
+class Base
+{
+public:
+    explicit Base(int d) : data(d)
+    {
+#ifndef NDEBUG
+        std::cout << "Base: " << data << std::endl;
+#endif
+    }
+
+    virtual ~Base()
+    {
+#ifndef NDEBUG
+        std::cout << "~Base: " << data << std::endl;
+#endif
+    }
+
+    int data;
+};
+
+class Derived : public Base
+{
+public:
+    explicit Derived(int d) : Base(d)
+    {
+#ifndef NDEBUG
+        std::cout << "Derived: " << data << std::endl;
+#endif
+    }
+
+    ~Derived()
+    {
+#ifndef NDEBUG
+        std::cout << "~Derived: " << data << std::endl;
+#endif
+    }
+
+    int get() const { return this->data; }
+};
+
+TEST_CASE("Util, Memory", "[util][memory]")
+{
+    using BPtr = Euclid::ProPtr<Base>;
+    using DPtr = Euclid::ProPtr<Derived>;
+
+    SECTION("ctor default")
+    {
+        BPtr p;
+        REQUIRE(!p);
+        REQUIRE(p.get() == nullptr);
+        REQUIRE(!p.owns());
+    }
+
+    SECTION("ctor nullptr")
+    {
+        BPtr p(nullptr);
+        REQUIRE(!p);
+        REQUIRE(p.get() == nullptr);
+        REQUIRE(!p.owns());
+    }
+
+    SECTION("ctor data")
+    {
+        Base b1{ 1 };
+        auto b2 = new Base(2);
+
+        // non-owning automatic variable
+        {
+            BPtr p1(&b1, false);
+            REQUIRE(p1);
+            REQUIRE(p1.get() == &b1);
+            REQUIRE(!p1.owns());
+            REQUIRE(p1->data == 1);
+            REQUIRE((*p1).data == 1);
+        }
+
+        // non-owning dynamic memory
+        {
+            BPtr p2(b2, false);
+            REQUIRE(p2);
+            REQUIRE(p2.get() == b2);
+            REQUIRE(!p2.owns());
+            REQUIRE(p2->data == 2);
+            REQUIRE((*p2).data == 2);
+        }
+        REQUIRE(b2->data == 2);
+
+        // owning dynamic memory
+        {
+            BPtr p3(b2, true);
+            REQUIRE(p3);
+            REQUIRE(p3.get() == b2);
+            REQUIRE(p3.owns());
+            REQUIRE(p3->data == 2);
+            REQUIRE((*p3).data == 2);
+        }
+    }
+
+    SECTION("copy and move")
+    {
+        // move assignment
+        auto p1 = BPtr(new Base(1), true);
+        REQUIRE(p1);
+
+        // copy from nullptr
+        p1 = nullptr;
+        REQUIRE(!p1);
+
+        // move construct
+        auto p2(BPtr(new Base(2), true));
+        REQUIRE(p2);
+
+        // move from self
+        p2 = std::move(p2);
+        REQUIRE(p2);
+
+        // move from existing
+        p1 = std::move(p2);
+        REQUIRE(p1);
+        REQUIRE(!p2);
+    }
+
+    SECTION("move from derived type")
+    {
+        auto p1 = BPtr(new Base(1), true);
+        auto p2 = DPtr(new Derived(2), true);
+        REQUIRE(p2->get() == 2);
+
+        p1 = std::move(p2);
+        REQUIRE(p1->data == 2);
+        REQUIRE(dynamic_cast<Derived*>(p1.get())->get() == 2);
+
+        p1 = DPtr(new Derived(3), true);
+        REQUIRE(p1->data == 3);
+        REQUIRE(dynamic_cast<Derived*>(p1.get())->get() == 3);
+    }
+
+    SECTION("release and reset")
+    {
+        BPtr p1(new Base(1), true);
+        BPtr p2(new Base(2), true);
+
+        auto tmp = p1.release();
+        REQUIRE(tmp->data == 1);
+
+        p2.reset(tmp, true);
+        REQUIRE(p2->data == 1);
+    }
+
+    SECTION("swap")
+    {
+        BPtr p1(new Base(1), true);
+        BPtr p2(new Base(2), true);
+        BPtr p3(p2.get(), false);
+
+        Euclid::swap(p1, p1);
+        REQUIRE(p1->data == 1);
+
+        Euclid::swap(p1, p3);
+        REQUIRE(p1->data == 2);
+        REQUIRE(!p1.owns());
+        REQUIRE(p1.get() == p2.get());
+        REQUIRE(p3->data == 1);
+        REQUIRE(p3.owns());
+    }
+}
